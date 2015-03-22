1427f39ccec0ae9dda24b67d1a4ca92d56fddf00
NALEPA Emmanuel <nalepae@gmail.com>
2015-03-22 21:54:46 +0100

    [support_vector_machine] alphas, b and w are "protected"

--- support_vector_machine/svm.py
+++ support_vector_machine/svm.py
@@ -11,23 +11,52 @@ class SupportVectorMachine(object):
     """This class describe a support vector machine (SVM)"""
 
     def __init__(self, datas):
+        self.alphas_or_b_changed = True
+
         self.X = datas[:, :-1]
         self.y = datas[:, -1]
 
-        # Number of training examples
-        self.m = datas.shape[0]
+        # Number of training examples and features
+        (self.m, self.n) = datas.shape
 
-        # Create list of lagrange multipliers alpha, and initialise it to 0
-        self.alphas = np.zeros(self.m)
+        # Create list of lagrange multipliers _alphas, and initialise it to 0
+        self._alphas = np.zeros(self.m)
 
-        # Initialise threshold to 0
-        self.b = 0
+        # Initialise threshold _b to 0
+        self._b = 0
+
+        # Initialise hyperplane normal vector _w to 0
+        self._w = np.zeros(self.n)
+
+    def _get_alphas(self):
+        """Get _alphas"""
+        return self._alphas
+
+    def _set_alphas(self, alphas):
+        """Set _alphas"""
+        self.alphas_or_b_changed = True
+        self._alphas = alphas
+
+    def _get_b(self):
+        """Get _b"""
+        return self._b
+
+    def _set_b(self, b):
+        """Set _b"""
+        self.alphas_or_b_changed = True
+        self._b = b
 
     def _get_w(self):
         """Compute normal hyperplane vector"""
-        return self.X.T.dot(self.y * self.alphas)
+        if self.alphas_or_b_changed:
+            self._w = self.X.T.dot(self.y * self._alphas)
+            self.alphas_or_b_changed = False
+
+        return self._w
 
     w = property(_get_w)
+    alphas = property(_get_alphas, _set_alphas)
+    b = property(_get_b, _set_b)
 
     def h(self, vector):
         """Compute the hypothesis for the vector 'vector'"""
